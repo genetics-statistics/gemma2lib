be510cb3965150fb039bf9408644686f85af8fc2
Pjotr Prins <pjotr.public01@thebird.nl>
2020-08-30 08:06:49 +0100

    Simplify option handling with SimpleNamespace

--- gemma2/format/plink.py
+++ gemma2/format/plink.py
@@ -1,10 +1,12 @@
 import logging
+from types import SimpleNamespace
 
 from pandas_plink import read_plink
 
-def convert_plink(path: str, verbose: int, debug: bool):
+def convert_plink(path: str, options: SimpleNamespace):
+    verbose = options.verbose
     (bim,fam,bed) = read_plink(path, verbose=(True if verbose>1 else False))
-    if debug:
+    if options.debug:
         print("Debug view of PLINK\n")
         print("===> BED") # alleles/markers
         print(bim.head())
