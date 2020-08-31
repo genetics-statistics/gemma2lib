ce4cd4cec6ae57360c434c59f87b31de0e5d2fb7
Pjotr Prins <pjotr.public01@thebird.nl>
2020-08-31 10:43:09 +0100

    Adding compact and gzip

--- gemma2/format/plink.py
+++ gemma2/format/plink.py
@@ -13,24 +13,24 @@ def memory_usage():
     else:
         print(f"{round(mem / float(2 ** 20) )}Mb RAM used")
 
-def convert_plink(path: str, options: SimpleNamespace):
+def convert_plink(path: str, options: SimpleNamespace, compression_level: int):
     """Convert PLINK format to GEMMA2"""
     verbose = options.verbose
     (bim,fam,bed) = read_plink(path, verbose=(True if verbose>1 else False))
     m = bed.compute()
     if options.debug:
         print("Debug view of PLINK\n")
-        print("===> BIM") # alleles/markers
+        print("===> BIM alleles/markers")
         print(bim.head())
         print(bim.info())
-        print("===> FAM") # samples/phenotypes
+        print("===> FAM samples/phenotypes")
         print(fam.head())
         print(fam.info())
-        print("===> BED") # genotypes
+        print("===> BED genotypes")
         print(m)
         print(m[0:3,0:40])
+        print(bim.shape)
 
-    print(bim.shape)
     markers2,phenos2 = bim.shape
     inds2,phenos = fam.shape
     markers, inds = m.shape
@@ -43,18 +43,22 @@ def convert_plink(path: str, options: SimpleNamespace):
     genofn = basefn+"_geno.tsv"
     logging.info(f"Writing geno file {genofn}")
     translate = { 1.0: "A", 2.0: "B", 0.0: "H" }
-    with open(genofn, "w") as f:
-        f.write("marker")
+
+    import gzip
+    # content = b"Lots of content here"
+    with gzip.open(genofn+".gz", mode='wb', compresslevel=compression_level) as f:
+    # with open(genofn, "w") as f:
+        f.write("marker".encode())
         for i in range(inds):
-            f.write(f"\t{i}")
+            f.write(f"\t{i}".encode())
         for j in range(markers):
             markername = bim.snp[j]
-            f.write(f"\n{markername}")
+            f.write(f"\n{markername}\t".encode())
             if options.low_mem: # shaves 20%
                 for i in range(inds):
-                    f.write(f"\t{translate[m[j,i]]}")
+                    f.write(f"{translate[m[j,i]]}".encode())
             else:
-                f.write("\t".join([ translate[item] for item in m[j] ]))
+                f.write("".join([ translate[item] for item in m[j] ]).encode())
 
     # Write control file last
     import json
@@ -72,6 +76,7 @@ def convert_plink(path: str, options: SimpleNamespace):
           "B": 3
         },
         "geno_transposed": True,
+        "geno_compact": True
     }
     controlfn = basefn+".json"
     with open(controlfn, 'w') as cf:
