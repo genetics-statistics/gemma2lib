d85df91c9ad3a2a05df52ffbf9cf97c17806b47b
Pjotr Prins <pjotr.public01@thebird.nl>
2020-09-06 10:58:02 +0100

    Started on BIMBAM export

---
+++ gemma2/format/bimbam.py
@@ -0,0 +1,16 @@
+# GEMMA2 BIMBAM format support
+
+import json
+import gzip
+import logging
+import numpy as np
+import sys
+
+from types import SimpleNamespace
+from gemma2.utility.system import memory_usage
+
+from gemma2.format.rqtl2 import load_control, load_geno
+
+def write_bimbam(control):
+    raise Exception("YES")
+    pass
