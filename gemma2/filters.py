d1b41f7eab6440515c686a5874bc103e706c1477
Pjotr Prins <pjotr.public01@thebird.nl>
2020-09-24 11:50:01 +0100

    Filter on missingness

--- gemma2/filters.py
+++ gemma2/filters.py
@@ -6,20 +6,26 @@ from os.path import dirname, basename, isfile
 import sys
 from gemma2.utility.options import get_options_ns
 import gemma2.utility.safe as safe
-import gemma2.utility.data
+import gemma2.utility.data as data
 from gemma2.format.rqtl2 import load_control, iter_pheno, iter_geno, write_new_control
 
 def error(msg: str):
     raise ValueError(msg)
 
-def maf_filter(marker: str, maf_threshold: float, gs: list, na_strings: list) -> bool:
+def maf_filter(marker: str, maf_threshold: float, miss_threshold: float,
+               gs: list, na_strings: list) -> bool:
     """Pass maf threshold? FIXME: need to account for values.
     Returns True if SNP passes MAF threshold
     """
+    num = len(gs)
     realgs = list(filter(lambda g: not g in na_strings, gs))
     realnum = len(realgs)
-    missing = len(gs) - realnum
+    missing = num - realnum
     counter=collections.Counter(realgs)
+    miss_fract = missing/num
+    if miss_fract > miss_threshold:
+        logging.debug(f"Miss filter {miss_fract} fails {counter} at {marker}")
+        return False
     if len(counter) < 2:
         logging.debug(f"MAF filter {maf_threshold} fails {counter} at {marker}")
         return False
@@ -32,7 +38,7 @@ def maf_filter(marker: str, maf_threshold: float, gs: list, na_strings: list) ->
         logging.debug(f"MAF filter {maf_threshold} fails {counter} at {marker}")
     return ret
 
-def filters(controlfn: str, pheno_column: int, maf: float):
+def filters(controlfn: str, pheno_column: int, maf: float, miss: float):
     control = load_control(controlfn)
     ctrl = data.methodize(control)
     na_strings = ctrl.na_strings
@@ -42,6 +48,7 @@ def filters(controlfn: str, pheno_column: int, maf: float):
     count = -1 # FIXME, now comes with iter
 
     with safe.pheno_write_open() as p:
+        logging.debug(f"Filtering phenotypes on missing data (NA)")
         for num,row in iter_pheno(path+"/"+ctrl.pheno, header = True):
             count += 1
             if not row[pheno_column] in na_strings:
@@ -55,13 +62,15 @@ def filters(controlfn: str, pheno_column: int, maf: float):
     idx = idx[1:]
     markers = 0
     with safe.geno_write_open() as g:
+        logging.debug(f"Filtering genotypes on missing data at {miss}")
+        logging.debug(f"Filtering genotypes on maf at {maf}")
         g.write("\t".join(["marker"]+ids).encode())
         g.write("\n".encode())
         for num,marker,genos in iter_geno(path+"/"+ctrl.geno, header = False):
             gs = []
             for i in idx:
                 gs.append(genos[i-1])
-            if maf_filter(marker,maf,gs,na_strings):
+            if maf_filter(marker,maf,miss,gs,na_strings):
                 markers += 1
                 g.write(marker.encode())
                 g.write("\t".encode())
