81fddaf3a854c0e3f044dcbac16ae12d611ca3a2
Pjotr Prins <pjotr.public01@thebird.nl>
2020-10-02 11:49:13 +0100

    Minor allele counts pass

--- gemma2/compute/geno.py
+++ gemma2/compute/geno.py
@@ -20,37 +20,40 @@ def calc_maf(gs: list, na_strings: dict, h_str: str) -> float:
     Compute minor allele frequency.
 
     >>> calc_maf(["A","A","A","B","B","B","A","H"],{"NA": 1},"H")
-    0.375
+    0.4375
 
     >>> calc_maf(["B","B","B","A","A","A","B","H"],{"NA": 1},"H")
-    0.375
+    0.4375
 
     >>> calc_maf(["B","B","B","A","A","A","B","H","NA"],{"NA": 1},"H")
-    0.375
+    0.4375
 
     >>> calc_maf(["B","B","B","A","A","A","B","H","H","H","NA"],{"NA": 1},"H")
-    0.3
+    0.45
 
     >>> calc_maf(["B","B","B","A","A","A","B","H","H","H","H","NA"],{"NA": 1},"H")
-    0.36363636363636365
-
+    0.45454545454545453
 
     """
-    num = len(gs)
-    realgs = list(filter(lambda g: not g in na_strings, gs))
-    realnum = len(realgs)
-    missing = num - realnum
-    counter=collections.Counter(realgs)
-    # we take the second value which differs from GEMMA1 in the rare
-    # instance that we have enough Heterozygous - FIXME when we have
-    # genotype numbers - H should count by minor allele 50%.
-    minor_count = counter.most_common()[1][1]
+    # num = len(gs)
+    # realgs = list(filter(lambda g: not g in na_strings, gs))
+    # realnum = len(realgs)
+    # missing = num - realnum
+    # print(geno_translate_to_num(gs,na_strings,h_str))
+    values = list(filter(lambda g: g!=None,geno_translate_to_num(gs,na_strings,h_str)))
+    # print(values)
+    realnum = len(values)
+    # print(len(values),values)
+    minor_count = 0.0
+    for geno in values:
+        minor_count += geno/2.0
+    # print(minor_count,realnum)
     return minor_count/realnum
 
 def is_maf_fail(gs: list, maf_threshold: float, na_strings: dict) -> bool:
     return calc_maf(gs,na_strings) < maf_threshold
 
-def geno_translate_to_num(gs: list) -> list:
+def geno_translate_to_num(gs: list, na_strings: dict = {"NA": 1}, h_str: str = "H") -> list:
     """Translate (3 char) genotypes to minor allele frequencies
 
     >>> geno_translate_to_num(["A","A","A","B","B","H"])
@@ -59,10 +62,21 @@ def geno_translate_to_num(gs: list) -> list:
     >>> geno_translate_to_num(["A","A","A","B","B","H",None])
     [0.0, 0.0, 0.0, 2.0, 2.0, 1.0, None]
 
+    Or with NA
+
+    >>> geno_translate_to_num(["A","A","A","B","B","H","NA"])
+    [0.0, 0.0, 0.0, 2.0, 2.0, 1.0, None]
+
     We can reverse alleles and get the minor count
 
     >>> geno_translate_to_num(["B","B","B","A","A","H",None])
     [0.0, 0.0, 0.0, 2.0, 2.0, 1.0, None]
+
+    >>> geno_translate_to_num(["B","B","B","A","H","H",None])
+    [0.0, 0.0, 0.0, 2.0, 1.0, 1.0, None]
+
+    >>> geno_translate_to_num(["B","B","B","A","H","H",None,"NA"])
+    [0.0, 0.0, 0.0, 2.0, 1.0, 1.0, None, None]
 
     These should fail
 
@@ -77,8 +91,13 @@ def geno_translate_to_num(gs: list) -> list:
     AssertionError: Expected max 3 alleles for Counter({'B': 4, 'A': 1, 'H': 1, 'C': 1})
 
     """
-    values = filter(None, gs)
-    counter=collections.Counter(values)
+    # num = len(gs)
+    # realgs = list(filter(lambda g: not g in na_strings, gs))
+    # realnum = len(realgs)
+    # missing = num - realnum
+    # values = filter(None, gs)
+    realgs = list(filter(lambda g: g!=None and g!=h_str and not g in na_strings, gs))
+    counter=collections.Counter(realgs)
     # we take the second value which differs from GEMMA1 in the rare
     # instance that we have enough Heterozygous - FIXME when we have
     # genotype numbers - H should count by minor allele 50%.
@@ -86,12 +105,13 @@ def geno_translate_to_num(gs: list) -> list:
     count = counter.most_common()
     alleles = len(count)
     assert alleles>1, f"Expected multiple alleles for {counter}"
-    assert alleles<=3, f"Expected max 3 alleles for {counter}"
+    assert alleles<=2, f"Expected max 2 alleles for {counter}"
     trans = {}
     trans[count[0][0]] = 0.0 # Major allele
     if alleles > 1:
-        trans[count[1][0]] = 2.0 # Minor allele
-    if alleles > 2:
-        trans[count[2][0]] = 1.0 # Heterozygous
+        trans[count[1][0]] = 2.0 # Set minor allele
+    trans[h_str] = 1.0 # Set heterozygous
     trans[None] = None
+    for k in na_strings:
+        trans[k] = None
     return [ trans[g] for g in gs ]
